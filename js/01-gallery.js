7fed8fd149b6f71430077bd81def9ade62773ac3
Oleksandr Lopatin <nitapol77@gmail.com>
2023-08-12 18:00:39 +0300

    task1 completed

---
+++ js/01-gallery.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+//console.log(galleryItems);
+
+const ul = document.querySelector('gallery');
+
+const markup = galleryItems.map((g) => `<li class="gallery__item">
+<a class="gallery__link" href="${g.original}">
+<img class="gallery__image" src="${g.preview}" data-source="${g.original}" 
+alt="${g.description}"></a></li>`).join("");
+
+const ulGallery = document.querySelector('.gallery');
+ulGallery.insertAdjacentHTML('afterbegin',markup);
+
+ulGallery.addEventListener('click', (e) => { 
+    if (e.target.nodeName !== 'IMG') return;
+     e.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+    instance.show();
+
+});
+
